f33739da1b16b0cb8ebb867890d2510b29eee98c
thegreatcodeholio <ian@icarey.net>
2024-06-13 18:21:09 -0400

    moved uploading to openmhz, broadcastify, and RDIo to the beginning of call processor.
    Put them each in their own thread so there is no blocking.

--- lib/call_processor.py
+++ lib/call_processor.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import threading
 import time
 
 from lib.archive_handler import archive_files
@@ -26,6 +27,7 @@ def log_time(action_name, start_time):
 
 
 def process_call(system_config, mp3_file_path):
+    threads = []
     start_time = time.time()
     module_logger.info(f"Processing File {mp3_file_path}")
 
@@ -77,6 +79,29 @@ def process_call(system_config, mp3_file_path):
         m4a_exists = compress_audio(system_config.get("audio_compression", {}), mp3_file_path)
         action_start = log_time("MP3 to M4A Convert", action_start)
 
+    module_logger.debug(f"Timestamp from file - {epoch_timestamp}")
+    module_logger.debug(f"Timestamp now - {time.time()}")
+    module_logger.debug(f"File Duration - {duration_sec}")
+    module_logger.debug(f"Skew Created to Now - {time.time() - epoch_timestamp}")
+    module_logger.debug(f"Skew Created Minus Duration - {time.time() - epoch_timestamp + duration_sec}")
+
+    # OpenMHZ upload task
+    openmhz_thread = threading.Thread(target=upload_to_openmhz_task, args=(system_config, m4a_file_path, call_data))
+    threads.append(openmhz_thread)
+
+    # Broadcastify Calls upload task
+    broadcastify_thread = threading.Thread(target=upload_to_broadcastify_calls_task, args=(
+    system_config, m4a_file_path, call_data, epoch_timestamp, duration_sec))
+    threads.append(broadcastify_thread)
+
+    # RDIO upload tasks
+    for rdio in system_config.get("rdio_systems", []):
+        rdio_thread = threading.Thread(target=upload_to_rdio_task, args=(rdio, m4a_file_path, call_data))
+        threads.append(rdio_thread)
+
+    # start upload threads
+    for thread in threads:
+        thread.start()
 
     # Legacy Tone Detection
     for icad_detect in system_config.get("icad_tone_detect_legacy", []):
@@ -103,7 +128,7 @@ def process_call(system_config, mp3_file_path):
     if system_config.get("tone_detection", {}).get("enabled", 0) == 1:
         if talkgroup_decimal not in system_config.get("tone_detection", {}).get("allowed_talkgroups",
                                                                                 []) and "*" not in system_config.get(
-                "tone_detection", {}).get("allowed_talkgroups", []):
+            "tone_detection", {}).get("allowed_talkgroups", []):
             module_logger.debug(
                 f"<<Tone>> <<Detection>> Disabled for Talkgroup {call_data.get('talkgroup_tag') or call_data.get('talkgroup')}")
         else:
@@ -117,7 +142,7 @@ def process_call(system_config, mp3_file_path):
     if system_config.get("transcribe", {}).get("enabled", 0) == 1:
         if talkgroup_decimal not in system_config.get("transcribe", {}).get("allowed_talkgroups",
                                                                             []) and "*" not in system_config.get(
-                "transcribe", {}).get("allowed_talkgroups", []):
+            "transcribe", {}).get("allowed_talkgroups", []):
             module_logger.debug(
                 f"<<iCAD>> <<Transcribe>> <<Disabled>> for Talkgroup {call_data.get('talkgroup_tag') or call_data.get('talkgroup')}")
         else:
@@ -157,41 +182,12 @@ def process_call(system_config, mp3_file_path):
 
     # Send to Players
 
-    # Upload to OpenMHZ
-    if system_config.get("openmhz", {}).get("enabled", 0) == 1:
-        if m4a_exists:
-            openmhz_result = upload_to_openmhz(system_config.get("openmhz", {}),
-                                               m4a_file_path, call_data)
-
-            action_start = log_time("OpenMHZ Upload", action_start)
-
-        else:
-            module_logger.warning(f"No M4A file can't send to OpenMHZ")
-
-    # Upload to BCFY Calls
-    if system_config.get("broadcastify_calls", {}).get("enabled", 0) == 1:
-        if m4a_exists:
-            current_time = time.time()
-            module_logger.debug(f"Timestamp from file - {epoch_timestamp}")
-            module_logger.debug(f"Timestamp now - {current_time}")
-            module_logger.debug(f"File Duration - {duration_sec}")
-            module_logger.debug(f"Skew Created to Now - {current_time - epoch_timestamp}")
-            module_logger.debug(f"Skew Created Minus Duration - {current_time - epoch_timestamp + duration_sec}")
-
-            bcfy_calls_result = upload_to_broadcastify_calls(system_config.get("broadcastify_calls", {}), m4a_file_path,
-                                                             call_data)
-
-            action_start = log_time("Broadcastify Calls", action_start)
-
-        else:
-            module_logger.warning(f"No M4A file can't send to Broadcastify Calls")
-
     # Upload to iCAD Player
     if call_data.get("audio_m4a_url", "") and system_config.get("icad_player", {}).get("enabled", 0) == 1:
 
         if talkgroup_decimal not in system_config.get("icad_player", {}).get("allowed_talkgroups",
                                                                              []) and "*" not in system_config.get(
-                "icad_player", {}).get("allowed_talkgroups", []):
+            "icad_player", {}).get("allowed_talkgroups", []):
             module_logger.warning(
                 f"iCAD Player Disabled for Talkgroup {call_data.get('talkgroup_tag') or call_data.get('talkgroup_decimal')}")
         else:
@@ -200,26 +196,11 @@ def process_call(system_config, mp3_file_path):
             if icad_player_result:
                 module_logger.info(f"Upload to iCAD Player Complete")
 
-    # Upload to RDIO systems
-    for rdio in system_config.get("rdio_systems", []):
-        if rdio.get("enabled", 0) == 1:
-            if not m4a_exists:
-                module_logger.warning(f"No M4A file can't send to RDIO")
-                continue
-            try:
-                upload_to_rdio(rdio, m4a_file_path, call_data)
-                action_start = log_time(f"RDIO Upload {rdio.get('rdio_url')}", action_start)
-            except Exception as e:
-                continue
-        else:
-            module_logger.warning(f"RDIO system is disabled: {rdio.get('rdio_url')}")
-            continue
-
     # Upload to Alerting
     if system_config.get("icad_alerting", {}).get("enabled", 0) == 1:
         if talkgroup_decimal not in system_config.get("icad_alerting", {}).get("allowed_talkgroups",
                                                                                []) and "*" not in system_config.get(
-                "icad_alerting", {}).get("allowed_talkgroups", []):
+            "icad_alerting", {}).get("allowed_talkgroups", []):
             module_logger.warning(
                 f"iCAD Alerting Disabled for Talkgroup {call_data.get('talkgroup_tag') or call_data.get('talkgroup_decimal')}")
         else:
@@ -230,5 +211,56 @@ def process_call(system_config, mp3_file_path):
     if not system_config.get("keep_files"):
         audio_file_cleanup(mp3_file_path)
 
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
     total_time = time.time() - start_time
     module_logger.info(f"Processing Complete for {mp3_file_path} - Total time: {total_time:.2f} seconds.")
+
+
+def upload_to_openmhz_task(system_config, m4a_file_path, call_data):
+    try:
+        if system_config.get("openmhz", {}).get("enabled", 0) == 1:
+            if m4a_file_path:
+                result = upload_to_openmhz(system_config.get("openmhz", {}), m4a_file_path, call_data)
+                log_time("OpenMHZ Upload", time.time())
+                return result
+            else:
+                module_logger.warning(f"No M4A file can't send to OpenMHZ")
+    except Exception as e:
+        module_logger.error(f"Error uploading to OpenMHZ: {e}")
+
+
+def upload_to_broadcastify_calls_task(system_config, m4a_file_path, call_data, epoch_timestamp, duration_sec):
+    try:
+        if system_config.get("broadcastify_calls", {}).get("enabled", 0) == 1:
+            if m4a_file_path:
+                current_time = time.time()
+                module_logger.debug(f"Timestamp from file - {epoch_timestamp}")
+                module_logger.debug(f"Timestamp now - {current_time}")
+                module_logger.debug(f"File Duration - {duration_sec}")
+                module_logger.debug(f"Skew Created to Now - {current_time - epoch_timestamp}")
+                module_logger.debug(f"Skew Created Minus Duration - {current_time - epoch_timestamp + duration_sec}")
+
+                result = upload_to_broadcastify_calls(system_config.get("broadcastify_calls", {}),
+                                                      m4a_file_path, call_data)
+                log_time("Broadcastify Calls", time.time())
+                return result
+            else:
+                module_logger.warning(f"No M4A file can't send to Broadcastify Calls")
+    except Exception as e:
+        module_logger.error(f"Broadcastify Calls Upload to Broadcastify Failed: {e}")
+
+
+def upload_to_rdio_task(rdio, m4a_file_path, call_data):
+    try:
+        if rdio.get("enabled", 0) == 1:
+            if m4a_file_path:
+                result = upload_to_rdio(rdio, m4a_file_path, call_data)
+                log_time(f"RDIO Upload {rdio.get('rdio_url')}", time.time())
+                return result
+            else:
+                module_logger.warning(f"No M4A file can't send to RDIO")
+    except Exception as e:
+        module_logger.error(f"Error uploading to RDIO {rdio.get('rdio_url')}: {e}")
